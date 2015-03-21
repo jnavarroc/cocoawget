c1870541ae87b791c239a4b5740238680bbb93ae
Javier Navarro <xaviernavarrocerda@gmail.com>
2015-03-21 18:44:36 +0100

    Update buildwget.sh
    update wget version

--- buildwget.sh
+++ buildwget.sh
@@ -2,12 +2,10 @@
 
 #http://developer.apple.com/technotes/tn2005/tn2137.html
 
-./wget -c http://ftp.gnu.org/gnu/wget/wget-1.11.4.tar.gz
-tar xvfz wget-1.11.4.tar.gz
-cd wget-1.11.4
+./wget -c http://ftp.gnu.org/gnu/wget/wget-1.15.tar.gz
+tar xvfz wget-1.15.tar.gz
+cd wget-1.15
 
-export CFLAGS="-Os -isysroot /Developer/SDKs/MacOSX10.5.sdk -arch i386 -arch ppc"  
-export LDFLAGS="-Wl,-syslibroot,/Developer/SDKs/MacOSX10.5.sdk" 
 ./configure --disable-dependency-tracking
 make
 cp src/wget ../
